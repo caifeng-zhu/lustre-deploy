60da17a04359bf3e428746d5f211896bbeb5f21c
zhongyuling <zhongyuling@ebtech.com>
2024-06-20 23:02:10 +0800

    Improve nvmet.sh
    1. Add debug option.
    2. Skip link if the linked file already exists.
    3. Clarify variable name, eg: config file, nvmet/node path.
    4. Add abbreviation list/ls, clear/clr, create/export.

--- nvmet.sh
+++ nvmet.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#TODO:
-# - debug as option
-# - config file as option
-#
-nvmet_config_path="/sys/kernel/config/nvmet"
-nvmet_work_path=
-nvmet_debug=0
+nvmet_path="/sys/kernel/config/nvmet"
+node_path=
+debug=0
 dryrun=
+
+config_file="./nvmet.conf"
 
 declare -a nvmet_port
 declare -a nvmet_disk
 
 debug_print() {
-	[ $nvmet_debug -ne 0 ] && printf "%*s $nvmet_work_path\n" $(($1 * 4)) $2
+	[ $debug -ne 0 ] && printf "%*s $node_path\n" $(($1 * 4)) $2
 }
 
 errexit() {
@@ -22,26 +20,32 @@ errexit() {
 }
 
 usage() {
-	less -F <<EOF
-${0##*/} create [-n --dryrun]
-${0##*/} ls [-n --dryrun]
-${0##*/} clear [-n --dryrun]
+cat <<EOF
+Usage: $(basename "$0") [-h] [-n] [-d] <command> [config file]
+Options:
+ -h	Display this help message
+ -n	Dry run mode (echo commands instead of executing them)
+ -d	Enable debug mode
+Commands:
+ create|export <config file>
+ clear|clr
+ list|ls
 EOF
-	exit 1
+	exit 0
 }
 
 node_create() {
 	local depth ndtype ndname ndval
-	local saved_path=$nvmet_work_path
+	local saved_path=$node_path
 
 	read -r depth ndtype ndname ndval  <<< $*
-	nvmet_work_path=$nvmet_work_path/$ndname
+	node_path=$node_path/$ndname
 	debug_print $depth '=>'
 
 	case $ndtype in
 	PORT)
-		if [ ! -e $nvmet_work_path ]; then
-			$dryrun mkdir $nvmet_work_path
+		if [ ! -e $node_path ]; then
+			$dryrun mkdir $node_path
 
 			read -r traddr trsvcid trtype <<< $ndval
 			node_create $((depth + 1)) ATTR addr_adrfam ipv4
@@ -52,15 +56,15 @@ node_create() {
 		;;
 
 	SUBSYS)
-		if [ ! -e $nvmet_work_path ]; then
-			$dryrun mkdir $nvmet_work_path
+		if [ ! -e $node_path ]; then
+			$dryrun mkdir $node_path
 			node_create $((depth + 1)) ATTR attr_allow_any_host 1
 		fi
 		;;
 
 	NS)
-		if [ ! -e $nvmet_work_path ]; then
-			$dryrun mkdir $nvmet_work_path
+		if [ ! -e $node_path ]; then
+			$dryrun mkdir $node_path
 
 			uuidpath=/sys/block/${ndval##*/}/uuid
 			node_create $((depth + 1)) ATTR device_path $ndval
@@ -76,10 +80,10 @@ node_create() {
 		# Attriute files are created automatically as empty by the
 		# driver. Here they are set with values from user space.
 		if [ -z "$dryrun" ]; then
-			echo $ndval > $nvmet_work_path
+			echo $ndval > $node_path
 		else
 			printf "%*s echo %8s > %s\n" $((depth * 4)) ' ' \
-				$ndval $nvmet_work_path
+				$ndval $node_path
 		fi
 		;;
 
@@ -89,20 +93,20 @@ node_create() {
 	esac
 
 	debug_print $depth '<='
-	nvmet_work_path=$saved_path
+	node_path=$saved_path
 }
 
 node_clear() {
-	local saved_path=$nvmet_work_path
+	local saved_path=$node_path
 	local ndtype ndname depth
 
 	read -r depth ndtype ndname <<< $*
-	nvmet_work_path=$nvmet_work_path/$ndname
+	node_path=$node_path/$ndname
 	debug_print $depth '=>'
 
 	case $ndtype in
 	PORTS)
-		for p in $(ls $nvmet_work_path); do
+		for p in $(ls $node_path); do
 			node_clear $((depth + 1)) PORT ${p##*/}
 		done
 		;;
@@ -110,35 +114,35 @@ node_clear() {
 	PORT)
 		node_clear $((depth + 1)) SUBSYSTEMS subsystems
 
-		$dryrun rmdir $nvmet_work_path
+		$dryrun rmdir $node_path
 		;;
 
 	SUBSYSTEMS)
-		for s in $(ls $nvmet_work_path); do
+		for s in $(ls $node_path); do
 			node_clear $((depth + 1)) SUBSYS ${s##*/}
 		done
 		;;
 
 	SUBSYS)
-		if [ -h $nvmet_work_path ]; then
-			$dryrun unlink $nvmet_work_path
-		elif [ -d $nvmet_work_path ]; then
+		if [ -h $node_path ]; then
+			$dryrun unlink $node_path
+		elif [ -d $node_path ]; then
 			node_clear $((depth + 1)) NAMESPACES namespaces
 
-			$dryrun rmdir $nvmet_work_path
+			$dryrun rmdir $node_path
 		else
-			errexit "unexpected path $nvmet_work_path"
+			errexit "unexpected path $node_path"
 		fi
 		;;
 
 	NAMESPACES)
-		for n in $(ls $nvmet_work_path); do
+		for n in $(ls $node_path); do
 			node_clear $((depth + 1)) NS ${n##*/}
 		done
 		;;
 
 	NS)
-		$dryrun rmdir $nvmet_work_path
+		$dryrun rmdir $node_path
 		;;
 
 	*)
@@ -147,15 +151,15 @@ node_clear() {
 	esac
 
 	debug_print $depth '<='
-	nvmet_work_path=$saved_path
+	node_path=$saved_path
 }
 
 node_show() {
-	local saved_path=$nvmet_work_path
+	local saved_path=$node_path
 	local ndtype ndname depth
 
 	read -r depth ndtype ndname <<< $*
-	nvmet_work_path=$nvmet_work_path/$ndname
+	node_path=$node_path/$ndname
 
 	debug_print $depth '=>'
 	printf "%*s" $((depth * 4)) ' '
@@ -165,7 +169,7 @@ node_show() {
 		$dryrun printf "$ndname:\n"
 
 		local p
-		for p in $(ls $nvmet_work_path); do
+		for p in $(ls $node_path); do
 			node_show $((depth + 1)) PORT ${p##*/}
 		done
 		;;
@@ -185,13 +189,13 @@ node_show() {
 		$dryrun printf "$ndname:\n"
 
 		local s
-		for s in $(ls $nvmet_work_path); do
+		for s in $(ls $node_path); do
 			node_show $((depth + 1)) SUBSYS ${s##*/}
 		done
 		;;
 
 	SUBSYS)
-		if [ -h $nvmet_work_path ]; then
+		if [ -h $node_path ]; then
 			$dryrun printf "$ndname\n"
 		else
 			$dryrun printf "$ndname:\n"
@@ -204,7 +208,7 @@ node_show() {
 		$dryrun printf "$ndname:\n"
 
 		local n
-		for n in $(ls $nvmet_work_path); do
+		for n in $(ls $node_path); do
 			node_show $((depth + 1)) NS ${n##*/}
 		done
 		;;
@@ -219,7 +223,7 @@ node_show() {
 		;;
 
 	ATTR)
-		$dryrun printf "%-16s %s\n" "$ndname:" $(cat $nvmet_work_path)
+		$dryrun printf "%-16s %s\n" "$ndname:" $(cat $node_path)
 		;;
 
 	*)
@@ -228,44 +232,64 @@ node_show() {
 	esac
 
 	debug_print $depth '<='
-	nvmet_work_path=$saved_path
+	node_path=$saved_path
 }
 
 nvmet_create() {
-	nvmet_work_path=$nvmet_config_path
+	node_path=$nvmet_path
 	for i in ${!nvmet_disk[*]}; do
 		read -r devpath pt subsys ns <<< ${nvmet_disk[$i]}
 		node_create 1 PORT ports/$pt ${nvmet_port[$pt]}
 		node_create 1 SUBSYS subsystems/$subsys 0
 		node_create 1 NS subsystems/$subsys/namespaces/$ns $devpath
 
-		$dryrun ln -s $nvmet_config_path/subsystems/$subsys \
-		    $nvmet_config_path/ports/$pt/subsystems/$subsys
+		[ ! -e $nvmet_path/ports/$pt/subsystems/$subsys ] \
+			&& $dryrun ln -s $nvmet_path/subsystems/$subsys \
+		    $nvmet_path/ports/$pt/subsystems/$subsys
 	done
 }
 
 nvmet_clear() {
-	nvmet_work_path=$nvmet_config_path
+	node_path=$nvmet_path
 	node_clear 1 PORTS ports
 	node_clear 1 SUBSYSTEMS subsystems
 }
 
 nvmet_show() {
-	nvmet_work_path=$nvmet_config_path
+	node_path=$nvmet_path
 	node_show 1 PORTS ports
 	node_show 1 SUBSYSTEMS subsystems
 }
 
-source ./nvmet.conf
-
-if [[ $2 == "-n" || $2 == "--dryrun" ]]; then
-	dryrun=echo
-fi
+while getopts "hnd" opt; do
+	case $opt in
+	h)	usage;;
+	n)	dryrun=echo;;
+	d)	debug=1;;
+	esac
+done
+shift $((OPTIND - 1))
 
 case $1 in
-	-h) 	usage;;
-	create) nvmet_create;;
-	clear) 	nvmet_clear;;
-	ls) 	nvmet_show;;
-	*) 	errexit "unknow options $1";;
+	create|export)
+		if [ ! -z $2 ]; then
+			config_file=$2
+		fi
+		if [ ! -e $config_file ]; then
+			errexit "invalid $config_file"
+		fi
+
+		source $config_file
+		nvmet_create
+		;;
+
+	clear|clr)
+		nvmet_clear
+		;;
+	list|ls)
+		nvmet_show
+		;;
+	*)
+		errexit "unknow options $1"
+		;;
 esac
