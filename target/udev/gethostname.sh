567ddca07b44fd3a6b6b8a59213046c858012d1c
caifeng zhu <caifengzhu@ebtech.com>
2025-07-24 14:24:13 +0800

    Add a udev rule to add device back into raid array.

--- target/udev/gethostname.sh
+++ target/udev/gethostname.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# this file is used by persistent-storage-*.rules to 
+# get a simple short name for cluster disk sharing. It
+# should be put in directory /etc.
+
 hostname=$(cat /etc/hostname)
 hostname=${hostname##*-}
 echo "n${hostname}"
