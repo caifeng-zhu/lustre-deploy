bdda748ab61be060f0a331154ab95d1a877f33e7
yulingzhong <yulingz@ebtech.com>
2025-09-09 17:12:39 +0800

    client deploy scripts

---
+++ client/cli.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import yaml
+import os
+import subprocess
+
+debug = False
+
+def run(cmd):
+    global debug
+
+    if debug:
+        print(f"=> debug {cmd}", flush=True)
+        return
+
+    print(f"=> {cmd}", flush=True)
+    try:
+        result = subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed: {cmd}\nExit code: {e.returncode}")
+        sys.exit(e.returncode)
+    if result.returncode != 0:
+        print(f"Command failed: {cmd}\nExit code: {result.returncode}")
+        sys.exit(result.returncode)
+
+    print("", flush=True)
+
+
+class ClientDispacher:
+    def __init__(self, addrs, workdir, agent):
+        self.hosts = addrs
+        self.workdir = workdir
+        self.agent = agent
+
+    def workfile(self, origfile):
+        return f'{self.workdir}/' + os.path.basename(origfile)
+
+    def start(self):
+        run(f'clush -qS -w {self.hosts} -b mkdir -p {self.workdir}')
+        self.copy(self.agent)
+
+    def check_host(self, machine):
+        self.execute('host_check', machine)
+
+    def execute(self, opname, *args):
+        #opargs = ' '.join(f"'{arg}'" for arg in args)
+        opargs = ' '.join(args)
+        agent = self.workfile(self.agent)
+        run(f'clush -qS -w {self.hosts} -b {agent} {opname} {opargs}')
+
+    def copy(self, *files):
+        copyfiles = ' '.join(files)
+        run(f'clush -qS -w {self.hosts} --copy {copyfiles} --dest {self.workdir}')
+        return [ self.workfile(file) for file in files ]
+
+
+def dispacher_create(clicfg, hostaddrs):
+    dispacher = ClientDispacher(hostaddrs, clicfg['workdir'], clicfg['agent'])
+    dispacher.start()
+    if 'machine' in clicfg:
+        dispacher.check_host(clicfg['machine'])
+    return dispacher
+
+
+class ClientItem:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def install(self, dispacher):
+        pass
+
+    def uninstall(self, dispacher):
+        pass
+
+    def start(self, dispacher):
+        pass
+
+    def stop(self, dispacher):
+        pass
+
+    def check(self, dispacher):
+        pass
+
+    def dump(self, dispacher):
+        pass
+
+
+class ClientPkgs(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def pkgs(self):
+        return self.cfg
+
+    def install(self, dispacher):
+        def copy(pkg):
+            if pkg.endswith('.deb'):
+                return dispacher.copy(pkg)[0]
+            return pkg
+        dispacher.execute('apt_install', *map(copy, self.pkgs))
+
+    def uninstall(self, dispacher):
+        def getname(pkg):
+            if pkg.endswith('.deb'):
+                return os.path.basename(pkg).partition('_')[0]
+            return pkg
+        dispacher.execute('apt_uninstall', *map(getname, self.pkgs))
+
+
+class ClientSubsys(ClientItem):
+    def __init__(self, cfg, parse_items, dumpcmd=''):
+        super().__init__(cfg)
+        self.parse_items = parse_items
+        self._items = None
+        self.dumpcmd = dumpcmd
+
+    @property
+    def items(self):
+        if self._items is None:
+            self._items = self.parse_items(self.cfg)
+        return self._items
+
+    def install(self, dispacher):
+        for item in self.items:
+            item.install(dispacher)
+
+    def uninstall(self, dispacher):
+        for item in self.items[::-1]:
+            item.uninstall(dispacher)
+
+    def start(self, dispacher):
+        for item in self.items:
+            item.start(dispacher)
+        for item in self.items:
+            item.check(dispacher)
+
+    def stop(self, dispacher):
+        for item in self.items[::-1]:
+            item.stop(dispacher)
+
+    def dump(self, dispacher):
+        for item in self.items:
+            item.dump(dispacher)
+        if self.dumpcmd:
+            dispacher.execute(self.dumpcmd)
+
+
+class LfsNetWorks(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def networks(self):
+        return [f"{net['net']} {net['nics']}" for net in self.cfg]
+
+    def install(self, dispacher):
+        dispacher.execute('lfs_add_networks', *self.networks)
+
+    def uninstall(self, dispacher):
+        dispacher.execute('lfs_del_networks', *self.networks)
+
+    def check(self, dispacher):
+        dispacher.execute('lfs_chk_networks', *self.networks)
+
+    def dump(self, dispacher):
+        dispacher.execute('lfs_dump_networks')
+
+
+class LfsRoutes(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def routes(self):
+        return [f"{route['net']} {route['nid']}" for route in self.cfg]
+
+    def install(self, dispacher):
+        dispacher.execute('lfs_add_routes', *self.routes)
+
+    def uninstall(self, dispacher):
+        #dispacher.execute('lfs_del_routes')
+        pass
+
+    def check(self, dispacher):
+        dispacher.execute(f'lfs_chk_routes', *self.routes)
+
+    def dump(self, dispacher):
+        dispacher.execute('lfs_dump_routes')
+
+
+class LfsMount(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def srcpath(self):
+        return self.cfg['srcpath']
+
+    @property
+    def dstpath(self):
+        return self.cfg['dstpath']
+
+    @property
+    def options(self):
+        return self.cfg['options']
+
+    def install(self, dispacher):
+        dispacher.execute('lfs_add_mount', self.srcpath, self.dstpath, self.options)
+
+    def uninstall(self, dispacher):
+        dispacher.execute('lfs_del_mount', self.dstpath)
+
+    def start(self, dispacher):
+        dispacher.execute('lfs_start_mount', self.srcpath, self.dstpath)
+
+    def stop(self, dispacher):
+        dispacher.execute('lfs_stop_mount', self.srcpath, self.dstpath)
+
+    def dump(self, dispacher):
+        # TODO
+        #dispacher.execute('lfs_dump_mount', self.srcpath, self.dstpath, self.options)
+        pass
+
+
+class LvmNvmets(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def nvmets(self):
+        return [f"{nvmet['traddr']} {nvmet['host-traddr']}" for nvmet in self.cfg]
+
+    def install(self, dispacher):
+        dispacher.execute("lvm_add_nvmets", *self.nvmets)
+
+    def uninstall(self, dispacher):
+        dispacher.execute("lvm_del_nvmets")
+
+    def start(self, dispacher):
+        dispacher.execute("lvm_start_nvmets")
+
+    def stop(self, dispacher):
+        dispacher.execute("lvm_stop_nvmets")
+
+    def dump(self, dispacher):
+        dispacher.execute('lvm_dump_nvmets')
+
+
+class LvmIscsits(ClientItem):
+    pass
+
+
+class LvmVg(ClientItem):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    @property
+    def vg(self):
+        return self.cfg
+
+    def install(self, dispacher):
+        dispacher.execute('lvm_add_vg', self.vg)
+
+    def uninstall(self, dispacher):
+        dispacher.execute('lvm_del_vg', self.vg)
+
+    def start(self, dispacher):
+        dispacher.execute('lvm_start_vg', self.vg)
+
+    def stop(self, dispacher):
+        dispacher.execute('lvm_stop_vg', self.vg)
+
+
+def parse_items_lfs_mounts(cfg):
+    return [LfsMount(mnt) for mnt in cfg]
+
+
+def parse_items_lfs(cfg):
+    items = []
+    items.append(ClientPkgs(cfg['pkgs']))
+    items.append(LfsNetWorks(cfg['networks']))
+    if 'routes' in cfg:
+        items.append(LfsRoutes(cfg['routes']))
+    items.append(ClientSubsys(cfg['mounts'], \
+            parse_items_lfs_mounts, 'lfs_dump_mounts'))
+    return items
+
+
+def parse_items_lvm_vgs(cfg):
+    return [LvmVg(vg) for vg in cfg]
+
+
+def parse_items_lvm(cfg):
+    items = []
+    items.append(ClientPkgs(cfg['pkgs']))
+    items.append(LvmNvmets(cfg['nvmets']))
+    items.append(ClientSubsys(cfg['vgs'],   \
+                              parse_items_lvm_vgs, 'lvm_dump_vgs'))
+    return items
+
+
+def parse_items_host(cfg):
+    items = []
+    if 'lfs' in cfg:
+        items.append(ClientSubsys(cfg['lfs'], parse_items_lfs))
+    if 'lvm' in cfg:
+        items.append(ClientSubsys(cfg['lvm'], parse_items_lvm))
+    return items
+
+
+def build(cfg, args):
+    dispacher = dispacher_create(cfg['client'], args.nodes)
+    cli = ClientSubsys(cfg, parse_items_host)
+    return dispacher, cli
+
+
+def main(argv):
+    ap = argparse.ArgumentParser('deploy client hosts')
+    ap.add_argument('-f', '--file', required=True, action='store')
+    ap.add_argument('-n', '--nodes', required=True, action='store')
+    ap.add_argument('--debug', required=False, action='store_true')
+    ap.add_argument(dest='operation', choices=['install', 'start', 'stop', 'uninstall', 'dump'],
+                    help="client deployment operation")
+    args = ap.parse_args(args=argv)
+    if args.debug:
+        debug = True
+
+    with open(args.file, 'r') as f:
+        config = yaml.safe_load(f)
+    dispatcher, cli = build(config, args)
+
+    if args.operation == 'install':     cli.install(dispatcher)
+    if args.operation == 'start':       cli.start(dispatcher)
+    if args.operation == 'stop':        cli.stop(dispatcher)
+    if args.operation == 'uninstall':   cli.uninstall(dispatcher)
+    if args.operation == 'dump':        cli.dump(dispatcher)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ client/cliagent.sh
@@ -0,0 +1,367 @@
+#!/bin/bash
+
+set -e
+
+debug=0
+
+runcmd() {
+	echo "-> $*" >&2
+	if [ $debug -eq 1 ]; then
+		return
+	fi
+	eval "$*"
+}
+
+errexit() {
+	printf "$*\n"
+	exit 1
+}
+
+cmd_run() {
+	runcmd $*
+}
+
+cmd_exist() {
+	local cmd=$1
+	which $cmd | wc -l
+}
+
+apt_install() {
+	runcmd apt install -y $*
+}
+
+apt_uninstall() {
+	runcmd apt remove -y $*
+}
+
+lfs_add_networks() {
+	local net nics cfgval
+
+	cfgval=""
+	while [ ${#*} -gt 0 ]; do
+		net=$1
+		nics=$2
+		shift 2
+		cfgval+="$net($nics),"
+	done
+	cfgval=${cfgval%,}
+
+	echo "options lnet networks=$cfgval" | tee /etc/modprobe.d/lustre.conf
+}
+
+lfs_del_networks() {
+	local cfgpath=/etc/modprobe.d/lustre.conf
+
+	if [ -e /sys/module/lustre ]; then
+		runcmd rmmod lustre
+	fi
+
+	if [ -e $cfgpath ]; then
+		runcmd rm -f $cfgpath
+	fi
+}
+
+lfs_chk_networks() {
+	local net nics
+
+	while [ ${#*} -gt 0 ]; do
+		net=$1
+		nics=$2
+		shift 2
+
+		net=${net%[0-9]}
+		ipaddr=$(ip -4 -br addr show dev bond0 | awk '$2=="UP" {print $3}' | sed -e 's/\/.*$//g')
+		if [ -z $ipaddr ]; then
+			errexit "can't find ipaddr from $nics"
+		fi
+
+		n=$(lctl list_nids | grep "$ipaddr@$net" | wc -l)
+		if [ $n -ne 1 ]; then
+			errexit "list nids $ipaddr@$net error"
+		fi
+	done
+}
+
+lfs_dump_networks() {
+	runcmd lctl list_nids
+}
+
+lfs_add_routes() {
+	local cfgval net nid
+
+	cfgval=""
+	while [ ${#*} -gt 0 ]; do
+		net=$1
+		nid=$2
+		shift 2
+		cfgval+="$net $nid;"
+	done
+	cfgval=${cfgval%;}
+
+	echo "options lnet routes=\"$cfgval\"" | tee -a /etc/modprobe.d/lustre.conf
+}
+
+lfs_del_routes() {
+	return
+}
+
+lfs_chk_routes() {
+	local net nid
+
+	while [ ${#*} -gt 0 ]; do
+		net=$1
+		nid=$2
+		shift 2
+
+		n=$(lnetctl route show --net $net --gateway $nid | grep "gateway: $nid" | wc -l)
+		if [ $n -eq 0 ]; then
+			errexit "show route net=$net gateway=$nid error"
+		fi
+	done
+}
+
+lfs_dump_routes() {
+	runcmd lnetctl route show
+}
+
+lfs_add_mount() {
+	local srcpath=$1
+	local dstpath=$(realpath $2)
+	local options=$3
+	local srvname=$(echo $dstpath | sed -e 's|^/||' -e 's|/|-|g')
+
+	cat > "/etc/systemd/system/$srvname.mount" <<EOF
+[Unit]
+Description=Mount ${srcpath} to ${dstpath}
+Requires=network-online.target
+After=network-online.target
+
+[Mount]
+What=${srcpath}
+Where=${dstpath}
+Type=lustre
+Options=${options}
+TimeoutSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+lfs_del_mount() {
+	local dstpath=$(realpath $1)
+	local srvname=$(echo $dstpath | sed 's|^/||; s|/|-|g')
+	local srvpath=/etc/systemd/system/$srvname.mount
+
+	if [ -e $srvpath ]; then
+		runcmd rm -f $srvpath
+	fi
+}
+
+lfs_find_mount() {
+	local srcpath=$1
+	local dstpath=$2
+
+	runcmd findmnt -n --source $srcpath --target $dstpath --type lustre | wc -l
+}
+
+lfs_start_mount() {
+	local srcpath=$1
+	local dstpath=$(realpath $2)
+	local srvname=$(echo $dstpath | sed 's|^/||; s|/|-|g')
+
+	runcmd systemctl enable $srvname.mount --now
+
+	n=$(lfs_find_mount $srcpath $dstpath)
+	if [ $n -eq 0 ]; then
+		errexit "mount $srcpath to $dstpath failed"
+	fi
+}
+
+lfs_stop_mount() {
+	local srcpath=$1
+	local dstpath=$(realpath $2)
+	local srvname=$(echo $dstpath | sed 's|^/||; s|/|-|g')
+	local srvpath=/etc/systemd/system/$srvname.mount
+
+	if [ -e $srvpath ]; then
+		runcmd systemctl disable $srvname.mount --now
+	fi
+
+	n=$(lfs_find_mount $srcpath $dstpath)
+	if [ $n -ne 0 ]; then
+		errexit "unmount $dstpath failed"
+	fi
+}
+
+lfs_dump_mounts() {
+	runcmd mount -t lustre
+}
+
+lvm_add_nvmets() {
+	local traddr htraddr append
+	local cfgline="--transport=rdma --trsvcid=4420 --nr-io-queues=4 --ctrl-loss-tmo=3 --reconnect-delay=1 --keep-alive-tmo=1"
+
+	append=""
+	while [ ${#*} -gt 0 ]; do
+		traddr=$1
+		htraddr=$2
+		shift 2
+
+		htraddr=$(ip -4 -br addr show to $htraddr | awk '$2=="UP" {print $3}' | sed -e 's/\/.*$//g')
+		if [ -z htraddr ]; then
+			errexit "can't find $htraddr"
+		fi
+
+		echo "$cfgline --traddr=$traddr --host-traddr=$htraddr" | tee $append /etc/nvme/discovery.conf
+		append='-a'
+	done
+}
+
+lvm_del_nvmets() {
+	runcmd rm -f /etc/nvme/discovery.conf
+}
+
+lvm_get_nvmets() {
+	# the local nvme nqn formart is nqn.2019-10.com.kioxia:KCM6XRUL3T84
+	# the remote nvme nqn format is n34-000:01:00.0
+	# so NQN!=nqn.* is remote nvme connection
+	runcmd nvme list-subsys | awk -F'NQN=' '{print $2}' | grep -v '^nqn\.'
+}
+
+lvm_start_nvmets() {
+	runcmd systemctl enable nvmf-autoconnect.service
+	runcmd nvme connect-all
+}
+
+lvm_stop_nvmets() {
+	if [ $(cmd_exist nvme) -eq 1 ]; then
+		runcmd nvme disconnect-all
+
+		if [[ $(lvm_get_nvmets) -ne 0 ]]; then
+			errexit 'nvme disconnect failed'
+		fi
+	fi
+	runcmd systemctl disable nvmf-autoconnect.service
+}
+
+lvm_dump_nvmets() {
+	if [ $(cmd_exist nvme) -eq 1 ]; then
+		runcmd nvme list-subsys
+	fi
+}
+
+lvm_add_vg() {
+	local vg=$1
+	local hn id idbase
+
+	hn=$(hostname)
+	case $hn in
+	gpu-a800-*)
+		idbase=1000
+		;;
+        gpu-h800-*)
+		idbase=0
+                ;;
+        cpu-*)
+		idbase=0	# for test
+                ;;
+        *)
+                errexit "hostname invalid"
+                ;;
+	esac
+	id=${hn##*-}
+	id=$((10#$id + idbase))	# distinguish a800 with h800
+
+	cat > "/etc/lvm/lvmlocal.conf" << EOF
+local {
+	host_id = $id
+}
+EOF
+}
+
+lvm_del_vg() {
+	runcmd "cat /dev/null > /etc/lvm/lvmlocal.conf"
+}
+
+lvm_start_vg() {
+	local vg=$1
+
+	if ! ls /dev/mapper/*-lvmlock > /dev/null 2>&1; then
+		runcmd systemctl disable wdmd --now
+
+		runcmd systemctl enable sanlock
+		runcmd systemctl restart sanlock
+		runcmd systemctl enable lvmlockd
+		runcmd systemctl restart lvmlockd
+		runcmd systemctl enable lvmagent.service --now
+	fi
+
+	runcmd vgchange --lockstart $vg
+	if [ ! -e /dev/mapper/$vg-lvmlock ]; then
+		errexit "start vg=$vg failed"
+	fi
+}
+
+lvm_stop_vg() {
+	local vg=$1
+
+	if [ -e /dev/mapper/$vg-lvmlock ]; then
+		runcmd vgchange -an $vg
+		runcmd vgchange --lockstop $vg
+	fi
+	if [ -e /dev/mapper/$vg-lvmlock ]; then
+		errexit "stop vg=$vg failed"
+	fi
+
+	runcmd systemctl disable lvmagent.service --now
+	runcmd systemctl disable lvmlockd --now
+	runcmd systemctl disable sanlock --now
+}
+
+lvm_dump_vgs() {
+	runcmd vgs
+	if [ $(cmd_exist lvmlockctl) -eq 1 ]; then
+		runcmd lvmlockctl -i
+	fi
+	if [ $(cmd_exist sanlock) -eq 1 ]; then
+		runcmd sanlock client status
+	fi
+}
+
+if [ "x$CLIENT_DEBUG" == "x1" ]; then
+	debug=1
+fi
+
+echo "$*"
+
+oper=$1; shift 1
+case $oper in
+'apt_install')			apt_install $*			;;
+'apt_uninstall') 		apt_uninstall $*		;;
+'cmd_run')			cmd_run $*			;;
+'lfs_add_networks')		lfs_add_networks $*		;;
+'lfs_del_networks')		lfs_del_networks $*		;;
+'lfs_chk_networks')		lfs_chk_networks $*		;;
+'lfs_dump_networks')		lfs_dump_networks $*		;;
+'lfs_add_routes')		lfs_add_routes $*		;;
+'lfs_del_routes')		lfs_del_routes $*		;;
+'lfs_chk_routes')		lfs_chk_routes $*		;;
+'lfs_dump_routes')		lfs_dump_routes $*		;;
+'lfs_add_mount')		lfs_add_mount $*		;;
+'lfs_del_mount')		lfs_del_mount $*		;;
+'lfs_start_mount')		lfs_start_mount $*		;;
+'lfs_stop_mount')		lfs_stop_mount $*		;;
+'lfs_dump_mounts')		lfs_dump_mounts $*		;;
+'lvm_add_nvmets')		lvm_add_nvmets $*		;;
+'lvm_del_nvmets')		lvm_del_nvmets $*		;;
+'lvm_start_nvmets')		lvm_start_nvmets $*		;;
+'lvm_stop_nvmets')		lvm_stop_nvmets $*		;;
+'lvm_dump_nvmets')		lvm_dump_nvmets $*		;;
+'lvm_add_vg')			lvm_add_vg $*			;;
+'lvm_del_vg')			lvm_del_vg $*			;;
+'lvm_start_vg')			lvm_start_vg $*			;;
+'lvm_stop_vg')			lvm_stop_vg $*			;;
+'lvm_dump_vgs')			lvm_dump_vgs $*			;;
+*)				errexit "unknown op: $oper"	;;
+esac
