2f7d604e8e93eebf3f4db9ef03844d1ed68d83bc
caifeng zhu <caifengzhu@ebtech.com>
2025-11-10 17:25:46 +0800

    Remove class ConfigData.

--- target/tgtconfig.py
+++ target/tgtconfig.py
@@ -16,47 +16,49 @@ def run(cmd: str):
         sys.exit(e.returncode)
 
 
-class ConfigData:
-    """
-    ConfigData is intended to simplify the config data access:
-    cfg['field'] is now can be read by cfgdt.field.
-    """
-    def __init__(self, cfg):
-        self.cfg = cfg
-
-    def __getattr__(self, name):
-        if self.cfg and name in self.cfg:
-            return self.cfg[name]
-        raise AttributeError(f'{name} not found')
-
-
 class ConfigAgent:
     def __init__(self, cfg):
-        self.cfgdt = ConfigData(cfg)
+        self.cfg = cfg
         self.client_cmds = []
 
+    @property
+    def mgmtip(self):
+        return self.cfg['mgmtip']
+
+    @property
+    def mode(self):
+        return self.cfg['mode']
+
+    @property
+    def workdir(self):
+        return self.cfg['workdir']
+
+    @property
+    def script(self):
+        return self.cfg['script']
+
     def workfile(self, origfile):
-        return f'{self.cfgdt.workdir}/' + os.path.basename(origfile)
+        return f'{self.workdir}/' + os.path.basename(origfile)
 
     def start(self):
-        run(f'ssh root@{self.cfgdt.mgmtip} mkdir -p {self.cfgdt.workdir}')
-        self.copy(self.cfgdt.script)
+        run(f'ssh root@{self.mgmtip} mkdir -p {self.workdir}')
+        self.copy(self.script)
 
     def execute(self, opname, *args):
-        mode = self.cfgdt.mode
+        mode = self.mode
         opargs = ' '.join(str(arg) for arg in args)
-        script = self.workfile(self.cfgdt.script)
-        run(f'ssh root@{self.cfgdt.mgmtip} {script} {mode} {opname} {opargs}')
+        script = self.workfile(self.script)
+        run(f'ssh root@{self.mgmtip} {script} {mode} {opname} {opargs}')
 
     def copy(self, *files):
         copyfiles = ' '.join(files)
-        run(f'scp {copyfiles} root@{self.cfgdt.mgmtip}:{self.cfgdt.workdir}')
+        run(f'scp {copyfiles} root@{self.mgmtip}:{self.workdir}')
         return [self.workfile(file) for file in files]
 
     @staticmethod
     def from_config(configs):
         agents = [ConfigAgent(cfg) for cfg in configs]
-        agents.sort(key=lambda a: a.cfgdt.mode)
+        agents.sort(key=lambda a: a.mode)
         for agent in agents:
             agent.start()
         return agents
